5a93910a3e354e8a038cc0bbe16de7c6ba63128b
Nina Y <nyuchi@risd.edu>
2020-09-30 22:44:27 -0400

    9.30

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-var show2020 = function(event){
-	var text = document.getElementById("about-text");
-	if (text.classList.contains("hide") == true){
-		text.style.height = "auto";
-		text.classList.remove("hide");
-	} else{
-		text.style.height = 0;
-		text.classList.add("hide");
-	};
-};
+// var show2020 = function(event){
+// 	var text = document.getElementById("about-text");
+// 	if (text.classList.contains("hide") == true){
+// 		text.style.height = "auto";
+// 		text.classList.remove("hide");
+// 	} else{
+// 		text.style.height = 0;
+// 		text.classList.add("hide");
+// 	};
+// }; 
+
+
+// CHANGE FOOTER YEAR HERE
+document.getElementById('year').innerHTML = "RISD IDSA 2020";
