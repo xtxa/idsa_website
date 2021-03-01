f285907ff4ed47f14cbcbf8b91bce623c5fcf5e8
Nina Y <nyuchi@risd.edu>
2021-03-01 12:55:15 -0500

    3.1

--- js/script.js
+++ js/script.js
@@ -11,4 +11,29 @@
 
 
 // CHANGE FOOTER YEAR HERE
-document.getElementById('year').innerHTML = "RISD IDSA 2020";
+document.getElementById('year').innerHTML = "RISD IDSA 2021";
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  var thisSlide = slides[slideIndex-1];
+  thisSlide.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+
+}
+
+//   setInterval(() => {
+//   plusSlides(1);
+// }, 7000);
